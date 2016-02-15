aae697f1df6082a5237bf2cedb16e7eb6a511118
ryo_shindo <ryo_shindo>
2016-02-15 19:01:43 +0900

    ファイルオープン・クローズ処理をItemStreamの実装メソッドに移譲

--- src/main/java/com/example/reader/EmployeeItemReader.java
+++ src/main/java/com/example/reader/EmployeeItemReader.java
@@ -26,29 +26,29 @@ import com.orangesignal.csv.annotation.CsvColumnException;
 import com.orangesignal.csv.io.CsvEntityReader;
 
 public class EmployeeItemReader implements ItemReader<Employee>, ItemStream {
-	
+
 	private String fileName;
-	
+
 	private CsvConfig config;
-	
+
 	private CsvEntityReader<Employee> entityReader;
-	
+
 	private static final Logger log = LoggerFactory.getLogger(EmployeeItemReader.class);
-	
+
 	private int lineCount = 0;
-	
+
 	private int restartIndex = 0;
-	
+
 	public EmployeeItemReader() {
 		init();
 	}
-	
+
 	private void init() {
 		config = new CsvConfig();
 		config.setIgnoreEmptyLines(true);
 		config.setVariableColumns(false);
 	}
-	
+
 	@BeforeStep
 	public void beforeStep(StepExecution stepExecution) {
 		log.info("[EmployeeItemReader] JOB_EXECUTION_ID : " + stepExecution.getJobExecutionId());
@@ -56,59 +56,37 @@ public class EmployeeItemReader implements ItemReader<Employee>, ItemStream {
 
 	@Override
 	public Employee read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
-		
-		Employee entity = null;
-		
-		try {
-			
-			if(entityReader == null) {
-				InputStreamReader in = new InputStreamReader(new FileInputStream(this.fileName), "MS932");
-				CsvReader csvReader = new CsvReader(in, config);
-				entityReader = CsvEntityReader.newInstance(csvReader, Employee.class);
-			}
 
-			entity = readLine();
-		
-		} catch(UnsupportedEncodingException e) {
-			throw new UnexpectedInputException("encoding not supported.", e);
-		} catch(FileNotFoundException e) {
-			throw new UnexpectedInputException("file not found", e);
-		} catch(IOException e) {
-			throw new NonTransientResourceException("Some problems have occured in file I/O.", e);
-		} finally {
-			// entityがnullであればEOFなので、確実にリソースの開放を行う.
-			if(entity == null && entityReader != null) {
-				entityReader.close();
-			}
-		}
-		
+		Employee entity = null;
+		entity = readLine();
 		return entity;
+
 	}
 
 	public void setFileName(String path) {
 		this.fileName = path;
 	}
-	
+
 	public void setSkipLines(int skipLines) {
 		config.setSkipLines(skipLines);
 	}
-	
+
 	/**
 	 * 一行を一行読み込み、POJOにマッピングして取得します.<br>
 	 * パースに失敗した場合は次の行を読み込んでPOJOを戻し、EOFだった場合はnullを戻します.
-	 * 
+	 *
 	 * @return　Employeeインスタンス
 	 * @throws IOException ファイル読み込みに異常が発生した場合
 	 */
 	private Employee readLine() throws IOException {
-		
+
 		Employee value = null;
 		boolean error = false;
 		try {
 			++lineCount;
 			value = entityReader.read();
 			if(value != null ) value.setLineNumber(lineCount);
-			
+
 			// リスタート位置までストリームを進める
 			if(lineCount <= restartIndex) {
 				value = readLine();
@@ -120,30 +98,47 @@ public class EmployeeItemReader implements ItemReader<Employee>, ItemStream {
 			log.warn("!! failed to parse a line " + lineCount + " : number of columns doesn't match.");
 			error = true;
 		}
-		
+
 		//パースに失敗した場合は次の行を読み込む
 		if(error) {
-			value = readLine();			
+			value = readLine();
 		}
-		
+
 		return value;
 	}
 
 	@Override
 	public void open(ExecutionContext executionContext) throws ItemStreamException {
+
+		try {
+			if(entityReader == null) {
+				InputStreamReader in = new InputStreamReader(new FileInputStream(this.fileName), "MS932");
+				CsvReader csvReader = new CsvReader(in, config);
+				entityReader = CsvEntityReader.newInstance(csvReader, Employee.class);
+			}
+		} catch(FileNotFoundException | UnsupportedEncodingException e) {
+			throw new ItemStreamException("cannot open file", e);
+		}
 		this.restartIndex = executionContext.getInt("partnerCsvLine", 0);
 	}
 
 	@Override
 	public void update(ExecutionContext executionContext) throws ItemStreamException {
 		executionContext.putInt("partnerCsvLine", lineCount);
-		
+
 	}
 
 	@Override
 	public void close() throws ItemStreamException {
+		try {
+			if(entityReader != null) {
+				entityReader.close();
+			}
+		} catch (IOException e) {
+			throw new ItemStreamException("cannot close file", e);
+		}
 	}
 
-	
+
 
 }
